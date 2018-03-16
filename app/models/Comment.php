4fee0f014d9370ed1892637b492bf2c4ccb62fb4
Jitendra Sharma <jitendra@atxlearning.com>
2018-03-16 21:35:15 +0530

    initial

---
+++ app/models/Comment.php
@@ -0,0 +1,9 @@
+<?php
+
+// app/models/Comment.php
+
+class Comment extends Eloquent { 
+	
+	$fillable = array('author', 'text'); 
+	
+}
---
+++ app/models/Item.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    //
+    protected $fillable = ['name', 'price'];
+    
+}
